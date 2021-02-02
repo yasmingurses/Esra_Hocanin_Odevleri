ea75de210f5aacd7cf066abcf99eaffd51334b29
Yasmin Gürses <72697857+yasmingurses@users.noreply.github.com>
2021-02-03 00:18:17 +0300

    Update 4 karakterli sifre.py

--- 4 karakterli sifre.py
+++ 4 karakterli sifre.py
@@ -1,11 +1,14 @@
+#Guncelleme
+#Rakamlardan olusup olusmadıgını da kontrol ediyor
+
 #4 karakterli sifre olusup olusmadıgını kontrol etme
 
 x = str(input("Dört karakterli bir şifre giriniz: "))
-while len(x) != 4 :
-    print("4 karakterli bir şifre olmalı")
+while len(x) != 4 or x.isdigit() == False :
+    print("4 karakterli ve rakamlardan oluşan bir şifre olmalı")
     x = str(input("Dört karakterli bir şifre giriniz: "))
 
-
-print("Girilen şifre: " , x)
+else:
+    print("Girilen şifre: " , x)
     
     
